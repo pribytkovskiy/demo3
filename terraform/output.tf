2de5ecd199551ad542a7268c749242b528e83931
Dima <dr.trem86@gmail.com>
2019-06-22 17:12:12 +0300

    front add address

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,3 @@
 output "public_ip" {
   value = "${google_compute_address.front-pool.address}"
 }
-
-output "instance_front_pool_ips" {
-  value = "${join(" ", google_compute_instance.front.*.network_interface.0.access_config.0.assigned_nat_ip)}"
-}
