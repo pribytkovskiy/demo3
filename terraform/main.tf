7a1a858e365fc19226481be47f28b5bd2a7de275
Dima <dr.trem86@gmail.com>
2019-06-25 21:55:25 +0300

    db3

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,29 +92,6 @@ resource "google_compute_instance" "back" {
 
 #Database
 
-resource "random_id" "db_name_suffix" {
-  byte_length = 4
-}
-
-resource "google_sql_database_instance" "db3" {
-  name = "db3"
-  database_version = "MYSQL_5_6"
-  region = "${var.region}"
-
-  settings {
-    tier = "db-n1-standard-2"
-  }
-}
-
-resource "google_sql_database" "db3" {
-  name      = "bike_championship"
-  instance  = "db3"
-
-  timeouts {
-    create = "2m"
-  }
-}
-
 resource "google_sql_user" "root" {
   name     = "root"
   instance = "db3"
